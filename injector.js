285e40dc69c726a37e49b370d5e444e280931d9c
Vanessa Arruda <varruda@sovrn.com>
2021-12-02 15:59:54 -0700

    working using element id

--- injector.js
+++ injector.js
@@ -1,5 +1,5 @@
-var element = document.getElementById("xDescIn");
+var element = document.getElementById("T2Ybvb23");
 const h1 = document.createElement("h1");
-h1.innerHTML = "<u>Hello World!</u>";
+h1.innerHTML = "<p><strong>Objective(s):</strong></p><p><br></p><p><strong>Meeting Coordinator:</strong></p><p><br></p><p><strong>Subject Matter Expert(s):</strong></p><p><br></p><p><strong>Meeting Material:</strong><br><br></p>";
 var textToAdd = document.createTextNode(h1);
 element.appendChild(h1);
