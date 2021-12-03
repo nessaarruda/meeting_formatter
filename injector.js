66346107a2ed00b28700fed7c251ccc5b44047b4
Vanessa Arruda <varruda@sovrn.com>
2021-12-03 13:24:18 -0700

    replace appendchild with modern syntax and r

--- injector.js
+++ injector.js
@@ -1,5 +1,4 @@
-var element = document.getElementById("xDescIn");
-const h1 = document.createElement("h1");
-h1.innerHTML = "<p><strong>Objective(s):</strong></p><p><br></p><p><strong>Meeting Coordinator:</strong></p><p><br></p><p><strong>Subject Matter Expert(s):</strong></p><p><br></p><p><strong>Meeting Material:</strong><br><br></p>";
-var textToAdd = document.createTextNode(h1);
-element.appendChild(h1);
+var element = document.activeElement;
+var h1 = document.createElement("h1");
+h1.innerHTML = "<p><strong>Objective(s):</strong></p><p><strong><br></strong></p><p><strong>Meeting Coordinator:</strong></p><p><strong><br></strong></p><p><strong>Subject Matter Expert(s):</strong></p><p><strong><br></strong></p><p><strong>Meeting Material:</strong></p>";
+element.append(h1);
