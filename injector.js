6f7545b511f72e6e9e7c7207fe27c58e11773a6f
Vanessa Arruda <varruda@sovrn.com>
2021-12-07 15:03:34 -0700

    fully functional on click

--- injector.js
+++ injector.js
@@ -1,4 +1,9 @@
 var element = document.activeElement;
 var h1 = document.createElement("h1");
-h1.innerHTML = "<p><strong><span style='font-size: 14px;'>Objective(s):</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Meeting Coordinator:</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Subject Matter Expert(s):</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Meeting Material:</span></strong></p>";
+h1.innerHTML = "<div style='font-weight:normal; font-size: 12px'>" +
+                    "<p><strong><span style='font-size: 14px;'>Objective(s):</span></strong></p>" +
+                    "<p><br></p><p><strong><span style='font-size: 14px;'>Meeting Coordinator:</span></strong></p>" +
+                    "<p><br></p><p><strong><span style='font-size: 14px;'>Subject Matter Expert(s):</span></strong></p>" +
+                    "<p><br></p><p><strong><span style='font-size: 14px;'>Meeting Material:</span></strong></p>" +
+                "</div>";
 element.append(h1);
