564feebc45be055cc2b244e93fe3cb172ab6e177
Vanessa Arruda <varruda@sovrn.com>
2021-12-07 11:13:58 -0700

    insert template on icon click

--- injector.js
+++ injector.js
@@ -1,4 +1,4 @@
 var element = document.activeElement;
 var h1 = document.createElement("h1");
-h1.innerHTML = "<p><strong><span style='font-size: 14px;'>Objective(s):</span></strong></p><p><br></p><p><strong><span style='font-size: 14px;'>Meeting Coordinator:</span></strong></p><p><br></p><p><strong><span style='font-size: 14px;'>Subject Matter Expert(s):</span></strong></p><p><br></p><p><strong><span style='font-size: 14px;'>Meeting Material:</span></strong></p>";
+h1.innerHTML = "<p><strong><span style='font-size: 14px;'>Objective(s):</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Meeting Coordinator:</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Subject Matter Expert(s):</span></strong></p><p></p><p><strong><span style='font-size: 14px;'>Meeting Material:</span></strong></p>";
 element.append(h1);
