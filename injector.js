55c15e8017aac7dca0e474cd6f950efe40786301
Vanessa Arruda <varruda@sovrn.com>
2021-12-08 17:24:27 -0700

    unfinished no access to user input

--- injector.js
+++ injector.js
@@ -1,6 +1,6 @@
-alert(document.getElementById("userInput1")
 var element = document.activeElement;
 var h1 = document.createElement("h1");
+// I want to string interpolate the user input so it is not hard coded
 h1.innerHTML = "<div style='font-weight:normal; font-size: 12px'>" +
                     "<p><strong><span style='font-size: 14px;'>Objective(s):</span></strong></p>" +
                     "<p><br></p><p><strong><span style='font-size: 14px;'>Meeting Material:</span></strong></p>" +
--- options.js
+++ options.js
@@ -1,10 +1,11 @@
 let button = document.querySelector("button");
 button.addEventListener('click', function(){
+  var input1 = document.getElementById("userInput1").value
+  var input2 = document.getElementById("userInput2").value
+  var input3 = document.getElementById("userInput3").value
+  var input4 = document.getElementById("userInput4").value
   chrome.tabs.create({
     url: 'https://calendar.google.com/calendar',
     active: true
   });
-  chrome.tabs.executeScript({
-    file: "injector.js"
-  });;
 });
